4a3c18d5f61dfeb4c1f75f3ee7c8cc1c3d9838af
kceron <karemiceron@gmail.com>
2021-05-22 00:25:36 -0400

    added songs

--- src/util.js
+++ src/util.js
@@ -3,12 +3,22 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Beaver Creek",
+      name: "Kinsfolk",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#205950", "#2ab3bf"],
+        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      artist: "Aarigod",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12997",
+      color: ["#A2BD6E", "#C4A95C"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Doin it",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Tesk",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8927",
+      color: ["#A27A5F", "#701B13"],
       id: uuidv4(),
       active: true,
     },
@@ -23,12 +33,32 @@ function chillHop() {
       active: false,
     },
     {
+      name: "By Chance",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      artist: "SwuM",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15224",
+      color: ["#484938", "#E3BB7F"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
       name: "Keep Going",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
       artist: "Swørn",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
       color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Oasis",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      artist: "Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
+      color: ["#0A4FB4", "#FEEC82"],
       id: uuidv4(),
       active: false,
     },
@@ -71,6 +101,26 @@ function chillHop() {
       color: ["ec8f56", "#8E766C"],
       id: uuidv4(),
       active: false,
+    },
+    {
+      name: "Through Trees",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      artist: "Sleepy Fish, The Field Tapes",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11251",
+      color: ["#A5B9BE", "#45689D"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Habitat",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      artist: "Philanthrope, Kendall Miles",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10265",
+      color: ["#414A2D", "#A15B39"],
+      id: uuidv4(),
+      active: true,
     },
     //ADD MORE HERE
   ];
