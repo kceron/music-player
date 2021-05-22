6351230486f9914f31e113445e1adce5797f323c
kceron <karemiceron@gmail.com>
2021-05-21 21:18:25 -0400

    library UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 import data from "./util";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
